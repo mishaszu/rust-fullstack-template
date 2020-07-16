8f03ae25f9b1f878c7c59ef11502b724da0d9ead
mishaszu <mishaszu@gmail.com>
2020-07-17 00:16:59 +0200

    Add support for SPA in actix-web

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,20 @@
-use actix_files::Files;
+use actix_files;
 use actix_web::http::StatusCode;
-use actix_web::{get, web, App, HttpResponse, HttpServer, Result};
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Result};
 use api::Hello;
 use serde_json;
 use std::env;
 
 #[get("/api/hello")]
-pub async fn index() -> Result<HttpResponse> {
+pub async fn hello() -> Result<HttpResponse> {
     let data = Hello::new("Hello from server");
     Ok(HttpResponse::build(StatusCode::OK)
         .content_type("application/json; charset=utf-8")
         .body(serde_json::to_value(data).unwrap()))
+}
+
+pub async fn index() -> Result<actix_files::NamedFile> {
+    Ok(actix_files::NamedFile::open("./static/index.html")?)
 }
 
 #[actix_rt::main]
@@ -21,13 +25,15 @@ async fn main() -> std::io::Result<()> {
         _ => "./static",
     };
     HttpServer::new(move || {
-        App::new().service(index).service(
-            web::scope("").default_service(
-                Files::new("", static_path)
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(hello)
+            .service(
+                actix_files::Files::new("/", static_path)
                     .index_file("index.html")
                     .use_last_modified(true),
-            ),
-        )
+            )
+            .default_service(web::resource("").route(web::get().to(index)))
     })
     .bind("0.0.0.0:8081")?
     .run()
